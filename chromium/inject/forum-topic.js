159004c66c81b24a872eda6bf6a1e51923c754d1
Erman <marioermando@gmail.com>
2015-04-11 15:20:48 +0200

    fixed the reply button thing

--- chromium/inject/forum-topic.js
+++ chromium/inject/forum-topic.js
@@ -1,19 +1,27 @@
-if(settings.forumReplyButton)
-	Array.prototype.forEach.call(document.querySelectorAll(".commentbox_1, .commentbox_2"), function(el){
-		var usrName = el.querySelector("a[href^='../members/view_profile.php?user=']").innerHTML,
-			replyButton = document.createElement("a");
+chrome.storage.sync.get("settings", function(i){
+	userSett = i.settings ? i.settings : defaultSettings; 
+	for (var sett in defaultSettings) {
+		if(!userSett.hasOwnProperty(sett)) userSett[sett] = defaultSettings[sett]; 
+	};
+	settings = userSett;
 
-		if(!el.querySelector(".edit_options"))
-			el.insertAdjacentHTML("afterbegin", "<span class='edit_options'></span>");
+	if(settings.forumReplyButton)
+		Array.prototype.forEach.call(document.querySelectorAll(".commentbox_1, .commentbox_2"), function(el){
+			var usrName = el.querySelector("a[href^='../members/view_profile.php?user=']").innerHTML,
+				replyButton = document.createElement("a");
 
-		replyButton.innerHTML = "Reply";
-		replyButton.href = "javascript:void(0)";
-		replyButton.onclick = function(){
-			$("textarea[name=message]").value += "[color=#00f]@"+ usrName + "[/color] ";
-			$("textarea[name=message]").focus();
-		}
+			if(!el.querySelector(".edit_options"))
+				el.insertAdjacentHTML("afterbegin", "<span class='edit_options'></span>");
 
-		el.querySelector(".edit_options").insertBefore(replyButton, el.querySelector(".edit_options").firstChild);
-		if(el.querySelector(".edit_options").children[1])
-			replyButton.insertAdjacentHTML("afterend", " ");
-	});
+			replyButton.innerHTML = "Reply";
+			replyButton.href = "javascript:void(0)";
+			replyButton.onclick = function(){
+				$("textarea[name=message]").value += "[color=#00f]@"+ usrName + "[/color] ";
+				$("textarea[name=message]").focus();
+			}
+
+			el.querySelector(".edit_options").insertBefore(replyButton, el.querySelector(".edit_options").firstChild);
+			if(el.querySelector(".edit_options").children[1])
+				replyButton.insertAdjacentHTML("afterend", " ");
+		});
+});
