b07ec5c2fc7dca27ed065ccbeea1d9819b680832
Erman <marioermando@gmail.com>
2015-04-12 01:44:25 +0200

    Added quotes to replies

--- firefox/data/inject/forum-topic.js
+++ firefox/data/inject/forum-topic.js
@@ -1,6 +1,30 @@
+var HTMLToBBCode = {
+	"(^<hr>|<hr>$)": "",
+	"\n": "",
+	"<center><div class=\"edit_warning\">(.*?)</div></center>": "", // get rid of the edit warning
+	"<br>": "\n",
+	"<hr>": "[line]",
+	"<b>": "[b]",
+	"</b>": "[/b]",
+	"<center>": "[center]",
+	"</center>": "[/center]",
+	"<img(?: class=\"user_image\"|) src=\"(.*)\">": function($0, $1){ return "[img]"+$1+"[/img]"; },
+	"<a href=\"(.*?)\" target=\"_blank\">(.*?)</a>": function($0, $1, $2){ return "[link="+$1+"]"+$2+"[/link]"; },
+	"<(?:.|\n)*?>": "" // dispose of unconverted tags
+}
+
+function convertHTMLToBBCode(html){
+	for(exp in HTMLToBBCode){
+		var regex = new RegExp(exp, 'gmi');
+		html = html.replace(regex, HTMLToBBCode[exp]);
+	}
+	return html;
+}
+
 if(settings.forumReplyButton)
 	Array.prototype.forEach.call(document.querySelectorAll(".commentbox_1, .commentbox_2"), function(el){
 		var usrName = el.querySelector("a[href^='../members/view_profile.php?user=']").innerHTML,
+			quote = convertHTMLToBBCode(el.innerHTML.match(/<hr>((.|\n)*)<hr>/)[1]),
 			replyButton = document.createElement("a");
 
 		if(!el.querySelector(".edit_options"))
@@ -9,7 +33,7 @@ if(settings.forumReplyButton)
 		replyButton.innerHTML = "Reply";
 		replyButton.href = "javascript:void(0)";
 		replyButton.onclick = function(){
-			$("textarea[name=message]").value += "[color=#00f]@"+ usrName + "[/color] ";
+			$("textarea[name=message]").value += "[box=#CCCCCC][blue][b]"+ usrName + "[/blue] said:[/b]\n[i]"+ quote + "[/i][/box]\n";
 			$("textarea[name=message]").focus();
 		}
 
