132fdbacf37bf5d1d84a6995ca6706945fc1f682
Erman <marioermando@gmail.com>
2015-04-17 14:07:09 +0200

    fixed a bug

--- chromium/inject/forum-cat.js
+++ chromium/inject/forum-cat.js
@@ -7,7 +7,8 @@ chrome.storage.sync.get("settings", function(i){
 
 	Array.prototype.forEach.call($$(".stickybox_1, .stickybox_2, .topicbox_1, .topicbox_2"), function(el){
 		if(settings.forumLastPageButton && el.querySelector('br')){
-			var lastPage = el.getElementsByTagName('A')[0].href += "&page=last";
+			var lastPage = el.getElementsByTagName('A')[0].href;
+			lastPage += "&page=last";
 			el.getElementsByTagName('BR')[0].insertAdjacentHTML("beforeBegin", " <a href='"+lastPage+"' style='font-size:10px'>[last page]</a>");
 		}
 	});
--- firefox/data/inject/forum-cat.js
+++ firefox/data/inject/forum-cat.js
@@ -1,6 +1,7 @@
 Array.prototype.forEach.call($$(".stickybox_1, .stickybox_2, .topicbox_1, .topicbox_2"), function(el){
 	if(settings.forumLastPageButton && el.querySelector('br')){
-		var lastPage = el.getElementsByTagName('A')[0].href += "&page=last";
+		var lastPage = el.getElementsByTagName('A')[0].href;
+		lastPage += "&page=last";
 		el.getElementsByTagName('BR')[0].insertAdjacentHTML("beforeBegin", " <a href='"+lastPage+"' style='font-size:10px'>[last page]</a>");
 	}
 });
