d24468284152761813c9666242651e59cb6a28d5
Erman <marioermando@gmail.com>
2015-05-04 16:45:36 +0200

    Last page button now only appears on threads with more than 1 page

--- chromium/inject/forum-cat.js
+++ chromium/inject/forum-cat.js
@@ -4,9 +4,10 @@ chrome.storage.sync.get("settings", function(i){
 		if(!userSett.hasOwnProperty(sett)) userSett[sett] = defaultSettings[sett]; 
 	};
 	settings = userSett;
-
+	
 	Array.prototype.forEach.call($$(".stickybox_1, .stickybox_2, .topicbox_1, .topicbox_2"), function(el){
-		if(settings.forumLastPageButton && el.querySelector('br')){
+		var ptsCount = el.innerHTML.match(/<\/a> \(([0-9]+)\)/) ? el.innerHTML.match(/<\/a> \(([0-9]+)\)/)[1] : 16;
+		if(settings.forumLastPageButton && el.querySelector('br') && ptsCount > 15){
 			var lastPage = el.getElementsByTagName('A')[0].href;
 			lastPage += "&page=last";
 			el.getElementsByTagName('BR')[0].insertAdjacentHTML("beforeBegin", " <a href='"+lastPage+"' style='font-size:10px'>[last page]</a>");
